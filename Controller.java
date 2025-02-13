d47e28eb2791dfe917910ce3832fd5edbd49dae6
DumbestPerson224 <eTmullall@outlook.com>
2025-02-13 15:37:49 -0500

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,9 @@
+public class Controller{
+    public static Controls getControllerLeft(){
+        return Controls.CONTROLLER_LEFT;
+    }
+
+    public Controls getControllerRight(){
+        return Controls.CONTROLLER_RIGHT;
+    }
+}
